21203b3f8815a4d4bd26db45a6addff45cfcad7c
cmccurd <cam.alex.mccurdy@gmail.com>
2021-09-20 12:26:42 -0400

    Finished getElements (continuing)

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,29 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+
+  var result = [];
+
+  var innerfunction = function (node) {
+    //if classname  === classname /push
+    if (node.classList === undefined) {
+
+    } else if (node.classList.value.includes(className)) {
+      result.push(node);
+      console.log(node.classList.value.includes(className));
+    }
+    //if has children loop
+    if (node.hasChildNodes) {
+      node.childNodes.forEach(function (item) {
+        innerfunction(item);
+      });
+    }
+  };
+  innerfunction(document.body);
+  return result;
 };
+
+
+
+// You should use document.body, element.childNodes, and element.classList
+
